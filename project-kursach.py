5aa6ed31e63a2ddaa2d686a3a26694aebc75911e
klenitskays <klenitskaya0916@gmail.com>
2024-04-09 11:59:57 +0300

    Beginning. adding two graphs

---
+++ project-kursach.py
@@ -0,0 +1,40 @@
+from flask import Flask, render_template
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import mpld3
+
+app = Flask(__name__)
+
+@app.route('/')
+def index():
+    # Загрузка данных
+    data = pd.read_csv('online_shoppers_intention.csv')
+
+    # Создание круговой диаграммы
+    revenue_counts = data['Revenue'].value_counts()
+    pie_chart = revenue_counts.plot(kind='pie', colors=sns.color_palette(), autopct='%1.1f%%')
+    plt.axis('equal')
+    plt.title('Доля совершенных покупок')
+
+    # Преобразование круговой диаграммы в интерактивный формат
+    pie_chart_html = mpld3.fig_to_html(pie_chart.figure)
+
+    # Создание графика столбцов
+    month_counts = data['Month'].value_counts()
+    column_chart = month_counts.plot(kind='bar', color=sns.color_palette())
+    plt.xlabel('Месяц')
+    plt.ylabel('Количество')
+    plt.title('Количество посещений в разные месяцы')
+
+    # Преобразование графика столбцов в интерактивный формат
+    column_chart_html = mpld3.fig_to_html(column_chart.figure)
+
+    # Преобразование данных в HTML-таблицу
+    table_html = data.head().to_html()
+
+    # Отображение результатов на веб-странице
+    return render_template('index.html', table=table_html, column_chart_html=column_chart_html, pie_chart_html=pie_chart_html)
+
+if __name__ == '__main__':
+    app.run()
