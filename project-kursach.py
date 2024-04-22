2b8870f993c9ad731c93d5926a9edab66df4c181
AnastassiaMazurenka <nastassiamazurenka@gmail.com>
2024-04-22 21:28:33 +0300

    Add 1 diagram with design

--- project-kursach.py
+++ project-kursach.py
@@ -1,10 +1,7 @@
 from flask import Flask, render_template
 import pandas as pd
-from sklearn.linear_model import LogisticRegression
-import plotly.graph_objs as go
-from plotly.subplots import make_subplots
-import matplotlib.pyplot as plt
-import mpld3
+import plotly.graph_objects as go
+from plotly.offline import plot
 
 app = Flask(__name__)
 app.static_folder = 'static'
@@ -14,49 +11,25 @@ def index():
     # Загрузка данных
     data = pd.read_csv('online_shoppers_intention.csv')
 
-    # Выделение признаков и целевой переменной
-    X = data[['Administrative', 'Informational']]
-    y = data['Revenue']
+    # Подсчет количества визитов или продуктовых запросов по месяцам
+    monthly_data = data.groupby('Month').size()
 
-    # Обучение модели логистической регрессии
-    model = LogisticRegression()
-    model.fit(X, y)
+    # Создание столбчатой диаграммы с использованием Plotly
+    bar_data = go.Bar(x=monthly_data.index, y=monthly_data.values)
 
-    # Вычисление предсказаний модели
-    predictions = model.predict(X)
+    bar_layout = go.Layout(
+        title='Столбчатая диаграмма по месяцам',
+        xaxis=dict(title='Месяц'),
+        yaxis=dict(title='Количество')
+    )
 
-    # Добавление предсказаний в DataFrame
-    data['Prediction'] = predictions
+    fig = go.Figure(data=[bar_data], layout=bar_layout)
 
-    # Создание розового графика
-    pink_line = [5, 10, 8, 3, 6, 9, 4]
-    plt.plot(pink_line, color='pink')
-    plt.xlabel('X')
-    plt.ylabel('Y')
-    plt.title('Розовый график')
+    # Преобразование диаграммы в HTML
+    plot_div = plot(fig, output_type='div')
 
-    # Преобразование розового графика в интерактивный формат
-    pink_graph_html = mpld3.fig_to_html(plt.gcf())
-
-    # Создание графика
-    fig = make_subplots(rows=2, cols=1, subplot_titles=['Revenue', 'Prediction'])
-
-    # График для реальных значений
-    fig.add_trace(go.Scatter(x=data.index, y=data['Revenue'], mode='markers', name='Revenue'), row=1, col=1)
-
-    # График для предсказаний
-    fig.add_trace(go.Scatter(x=data.index, y=data['Prediction'], mode='markers', name='Prediction'), row=2, col=1)
-
-    # Настройка макета графика
-    fig.update_layout(height=600, width=800, showlegend=False)
-
-    # Получение HTML-кода для встроенного графика
-    graph_html = fig.to_html(full_html=False)
-
-    css_file = 'style.css'
-
-    # Отображение результатов на веб-странице
-    return render_template('index.html', graph=graph_html, pink_graph=pink_graph_html, css_file=css_file)
+    # Отображение результатов на веб-странице и передача данных
+    return render_template('index.html', plot_div=plot_div, data=data.to_csv(index=False))
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True, port=8000, threaded=True)
---
+++ setup.py
@@ -0,0 +1,14 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='my_flask_app',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    install_requires=[
+        'Flask',
+        'pandas',
+        'matplotlib',
+        'mpld3'
+    ]
+)
